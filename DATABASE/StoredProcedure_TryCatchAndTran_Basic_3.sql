744b223a34ea1a652225f80e3388d0859fc0305f
Filipe Natanael <filipenatanael@live.com>
2017-03-21 11:18:32 -0300

    TryCatch and TRAN
    I created some exemples of TryCatch and Tran on SQL SERVER 2012.

---
+++ DATABASE/StoredProcedure_TryCatchAndTran_Basic_3.sql
@@ -0,0 +1,38 @@
+CREATE PROCEDURE uspTryCatchAndTran
+(
+    @Nome VARCHAR(100),
+	@CPF VARCHAR(12)
+)
+AS
+BEGIN
+   
+   BEGIN TRY
+      -- Iniciando transação de dados
+	  BEGIN TRAN
+	  -- Iniciando estrutura condifional
+	  IF(@Nome IS NOT NULL)
+	  BEGIN
+	  INSERT INTO PESSOAS(Nome,CPF) VALUES(@Nome,@CPF)
+	  END
+
+	  COMMIT TRAN
+	  
+	  SELECT @Nome AS Retorno
+	  
+   END TRY
+   BEGIN CATCH
+
+      ROLLBACK TRAN
+      SELECT ERROR_MESSAGE() AS Retorno
+
+   END CATCH
+
+
+END
+
+
+-- Testing Stored Procedure >> Exceção e Transação
+
+EXEC uspTryCatchAndTran 'Carlos Antonio', '564161613'
+
+SELECT * FROM PESSOAS
