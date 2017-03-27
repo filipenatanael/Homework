81c51455388189f894aa0bdef2648eb473727f0e
Filipe Natanael <filipenatanael@live.com>
2017-03-27 15:00:51 -0300

    Exercicio 10
    Inserir 3 registros na tabela Fornecedor e verificar os registros
    incluídos.

---
+++ Banco de dados/SQL_Exercise_L110.sql
@@ -0,0 +1,9 @@
+-- Inserir 3 registros na tabela Fornecedor e verificar os registros incluídos.
+
+INSERT INTO Fornecedor(nome,contato) VALUES('METAIS METRILA','31 6677-8899')
+
+INSERT INTO Fornecedor(nome,contato) VALUES('TIGRE CONEXOES','47 7788-9900')
+
+INSERT INTO Fornecedor(nome,contato) VALUES('GALVANIZADOS BRASIL','10 1112 3399')
+
+SELECT * FROM Fornecedor
