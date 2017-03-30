56b2fee55baf08eed3c8eae97a9155e723e24033
Filipe Natanael <filipenatanael@live.com>
2017-03-30 11:44:55 -0300

    Lista de exercicios POO 1
    Exercicios 3 ao 4

--- POO/LISTA1_POO/src/Exercicio_3_4/Quadrado.java
+++ POO/LISTA1_POO/src/Exercicio_3_4/Quadrado.java
@@ -2,7 +2,26 @@ package Exercicio_3_4;
 
 public class Quadrado {
 	private double lado;
-	private doble area;
-	private double
+	private double area;
+	private double perimetro;
+	
+	public Quadrado(double lado, double area, double perimetro){
+		this.lado = lado;
+		this.area = area;
+		this.perimetro = perimetro;
+	}
+	
+	public void calcularArea(){
+		this.area = this.lado * this.lado;
+	}
+	
+	public void calcularPerimetro(){
+		this.perimetro = 4 * this.lado;
+	}
+	
+	public void imprimir(){
+		System.out.print("Lado: "+this.lado+" Area: "+this.area+" perimetro: "+this.perimetro);
+	}
+	
 
 }
---
+++ POO/LISTA1_POO/src/Exercicio_3_4/Testando.java
@@ -0,0 +1,18 @@
+package Exercicio_3_4;
+
+public class Testando {
+
+	public static void main(String[] args) {
+		
+		//Quadrado q1 = new Quadrado();
+		//q1.imprimir();
+		
+		Quadrado q2 = new Quadrado(25,625,100);
+		q2.calcularArea();
+		q2.calcularPerimetro();
+		
+		q2.imprimir();
+
+	}
+
+}
