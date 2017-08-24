6552bf76d5344f3fa72967c9431183609f5a6ddd
Filipe Natanael <filipenatanael@live.com>
2017-08-23 23:36:27 -0300

    LEFT JOIN
    LEFT JOIN

--- DATABASE/RememberingSQLServer.sql
+++ DATABASE/RememberingSQLServer.sql
@@ -1,4 +1,6 @@
 SELECT * FROM Cliente AS C JOIN Pedido AS P ON C.codigoCli = P.codigoCli
 
+/*
 SELECT nome+' '+sobrenome AS 'NOME CLIENTE', cidade AS 'CIDADE DO CLIENTE', cpf AS 'NUMERO DO CPF'
 FROM Cliente AS C JOIN Pedido AS P ON C.codigoCli = P.codigoCli
+*/
