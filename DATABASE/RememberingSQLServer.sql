599b3abd6618058a320d28bab0a76aadfc9371b5
Filipe Natanael <filipenatanael@live.com>
2017-08-23 23:26:07 -0300

    Remembering 2
    Remembering 2

--- DATABASE/RememberingSQLServer.sql
+++ DATABASE/RememberingSQLServer.sql
@@ -1,1 +1,3 @@
 SELECT * FROM Cliente AS C JOIN Pedido AS P ON C.codigoCli = P.codigoCli
+
+SELECT nome+' '+sobrenome AS "NOME CLIENTE", cidade AS "CIDADE DO CLIENTE" FROM Cliente AS C JOIN Pedido AS P ON C.codigoCli = P.codigoCli
