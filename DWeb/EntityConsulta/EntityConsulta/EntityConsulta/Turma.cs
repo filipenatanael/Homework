c1f22f48d4a0f3e054116f738a8f414443597d08
Filipe Natanael <filipenatanael@live.com>
2017-09-04 23:42:04 -0300

    Desenvolvimento Web 1
    Codigo das aulas de desenvolvimento web usando .NET

---
+++ DWeb/EntityConsulta/EntityConsulta/EntityConsulta/Turma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityConsulta
+{
+    public class Turma
+    {
+
+        public Turma()
+        {
+            Alunos = new List<Aluno>();
+        }
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public List<Aluno> Alunos{ get; set; }
+    }
+}
