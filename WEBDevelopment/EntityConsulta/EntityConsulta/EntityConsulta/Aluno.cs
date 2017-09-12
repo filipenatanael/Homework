4a5a14aacc7a46787bfea41abd4d7c5568dca5d1
Filipe Natanael <filipenatanael@live.com>
2017-09-11 22:55:25 -0300

    Configuring the environment
    Configuring the environment

---
+++ WEBDevelopment/EntityConsulta/EntityConsulta/EntityConsulta/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityConsulta
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public decimal ValorMensalidade { get; set; }
+
+        public int TurmaId { get; set; }
+
+        public Turma Turma { get; set; }
+
+    }
+}
---
+++ WEBDevelopment/EntityConsulta/EntityConsulta/EntityConsulta/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityConsulta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Inicializar();
+
+            var aluno = new Aluno() { Nome = "Jose Antonio", DataNascimento = new DateTime(1979, 06, 27), ValorMensalidade = 2000, TurmaId = 2};
+            var ctx = new Contexto();
+            ctx.Alunos.Add(aluno);
+            ctx.SaveChanges();
+
+
+        }
+        static void Inicializar() {
+
+            var turma = new Turma(){Nome = "ADSBPAN2B"};
+
+            turma.Alunos.Add(new Aluno()
+            {
+                Nome = "Alex Sales",
+                DataNascimento = new DateTime(1989, 06, 27),
+                ValorMensalidade = 1500
+            });
+
+            turma.Alunos.Add(new Aluno()
+            {
+                Nome = "Julio Oliveira",
+                ValorMensalidade = 2498,
+                DataNascimento = new DateTime(1984, 7, 10),
+            });
+
+            var ctx = new Contexto();
+            ctx.Turmas.Add(turma);
+
+            ctx.SaveChanges();
+        }
+  
+
+
+
+    }
+}
