e0c57e3da4530e01f4b40fc24ad720ff1edd4569
Filipe Natanael <filipenatanael@live.com>
2017-09-24 23:57:19 -0300

    Created Customer Controller
    Created Customer Controller

--- WEBDevelopment/MyFirstAPI2/MyFirstAPI2/Models/Customer.cs
+++ WEBDevelopment/MyFirstAPI2/MyFirstAPI2/Models/Customer.cs
@@ -7,13 +7,17 @@ namespace MyFirstAPI2.Models
 {
     public class Customer
     {
+
+        public Customer()
+        {
+            listOfOrders = new List<Order>();
+        }
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public int ZipCode { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-
-
 
         public List<Order> listOfOrders { get; set; }
 
