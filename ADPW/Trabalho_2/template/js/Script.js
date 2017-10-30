a5754135bd9eab9736356a4c3ba3b00bf1e3231e
Filipe Natanael <filipenatanael@live.com>
2017-10-29 23:46:36 -0200

    Characterization of the project
    Testing and finishing this page

--- ADPW/Trabalho_2/template/js/Script.js
+++ ADPW/Trabalho_2/template/js/Script.js
@@ -1,9 +1,33 @@
 function FormSubmit() {
   var title_project = document.getElementById('title_project').value;
   var college_campus = document.getElementById('college_campus').value;
+  var director_campus = document.getElementById('director_campus').value;
+  var course_coordinator = document.getElementById('course_coordinator').value;
+  var knowledge_area = document.getElementById('knowledge_area').value;
+  var main_theme_Area1 = document.getElementById('main_theme_Area1').value;
+  var theme_Area2 = document.getElementById('theme_Area2').value;
+  var theme_Area3 = document.getElementById('theme_Area3').value;
+  var main_extension = document.getElementById('main_extension').value;
+  var extension2 = document.getElementById('extension2').value;
+  var extension3 = document.getElementById('extension3').value;
+  var radio_bond = document.getElementById('radio_bond').value;
+  var checkbox_sustainability = document.getElementById('checkbox_sustainability').value;
+  var Describe_interdisciplinarity = document.getElementById('Describe_interdisciplinarity').value;
 
+  console.log(title_project);
+  console.log(college_campus);
+  console.log(director_campus);
+  console.log(course_coordinator);
+  console.log(knowledge_area);
+  console.log(main_theme_Area1);
+  console.log(theme_Area2);
+  console.log(theme_Area3);
+  console.log(main_extension);
+  console.log(extension2);
+  console.log(extension3);
+  console.log(radio_bond);
+  console.log(checkbox_sustainability);
+  console.log(Describe_interdisciplinarity);
 
-
-  console.log(title_project+college_campus);
 
 }
