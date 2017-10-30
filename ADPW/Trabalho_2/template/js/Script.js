5438e12cbcc21f134dd4f547a639aa8f762f0dd6
Filipe Natanael <filipenatanael@live.com>
2017-10-29 23:57:22 -0200

    Using LocalStorage
    Using LocalStorage to save it

--- ADPW/Trabalho_2/template/js/Script.js
+++ ADPW/Trabalho_2/template/js/Script.js
@@ -1,33 +1,21 @@
 function FormSubmit() {
-  var title_project = document.getElementById('title_project').value;
-  var college_campus = document.getElementById('college_campus').value;
-  var director_campus = document.getElementById('director_campus').value;
-  var course_coordinator = document.getElementById('course_coordinator').value;
-  var knowledge_area = document.getElementById('knowledge_area').value;
-  var main_theme_Area1 = document.getElementById('main_theme_Area1').value;
-  var theme_Area2 = document.getElementById('theme_Area2').value;
-  var theme_Area3 = document.getElementById('theme_Area3').value;
-  var main_extension = document.getElementById('main_extension').value;
-  var extension2 = document.getElementById('extension2').value;
-  var extension3 = document.getElementById('extension3').value;
-  var radio_bond = document.getElementById('radio_bond').value;
-  var checkbox_sustainability = document.getElementById('checkbox_sustainability').value;
-  var Describe_interdisciplinarity = document.getElementById('Describe_interdisciplinarity').value;
 
-  console.log(title_project);
-  console.log(college_campus);
-  console.log(director_campus);
-  console.log(course_coordinator);
-  console.log(knowledge_area);
-  console.log(main_theme_Area1);
-  console.log(theme_Area2);
-  console.log(theme_Area3);
-  console.log(main_extension);
-  console.log(extension2);
-  console.log(extension3);
-  console.log(radio_bond);
-  console.log(checkbox_sustainability);
-  console.log(Describe_interdisciplinarity);
+  window.localStorage.setItem('title_project', document.getElementById('title_project').value);
+  window.localStorage.setItem('college_campus', document.getElementById('college_campus').value);
+  window.localStorage.setItem('director_campus', document.getElementById('director_campus').value);
+  window.localStorage.setItem('course_coordinator', document.getElementById('course_coordinator').value);
+  window.localStorage.setItem('knowledge_area', document.getElementById('knowledge_area').value);
+  window.localStorage.setItem('main_theme_Area1', document.getElementById('main_theme_Area1').value);
+  window.localStorage.setItem('theme_Area2', document.getElementById('theme_Area2').value);
+  window.localStorage.setItem('theme_Area3', document.getElementById('theme_Area3').value);
+  window.localStorage.setItem('main_extension', document.getElementById('main_extension').value);
+  window.localStorage.setItem('extension2', document.getElementById('extension2').value);
+  window.localStorage.setItem('extension3', document.getElementById('extension3').value);
+  window.localStorage.setItem('radio_bond', document.getElementById('radio_bond').value);
+  window.localStorage.setItem('checkbox_sustainability', document.getElementById('checkbox_sustainability').value);
+  window.localStorage.setItem('Describe_interdisciplinarity', document.getElementById('Describe_interdisciplinarity').value);
+
+  alert(window.localStorage.getItem('title_project'));
 
 
 }
