4d4efbef3cb599e81878a75652f05ad0f8b0c2a3
Filipe Natanael <filipenatanael@live.com>
2017-11-22 23:59:08 -0200

    Created table demands
    Created table for demands

--- ADPW/TrabalhoBootstrap/bootstrap/js/script.js
+++ ADPW/TrabalhoBootstrap/bootstrap/js/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-
   $( "#target" ).submit(function( event ) {
     window.localStorage.setItem('email',  $('#email').val());
     window.localStorage.setItem('password',  $('#password').val());
-    alert("Email: "+window.localStorage.getItem('email')+" Password: " +password);
     event.preventDefault();
     $(location).attr('href', 'home.html');
   });
-
 });
