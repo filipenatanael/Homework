89dae7b3cf5ff6fa8e805627ffec24c3a8ecb96c
Filipe Natanael <filipenatanael@live.com>
2017-11-23 01:35:58 -0200

    Table settings and buttons
    Table settings and buttons

--- ADPW/TrabalhoBootstrap/bootstrap/js/script.js
+++ ADPW/TrabalhoBootstrap/bootstrap/js/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
   $( "#target" ).submit(function( event ) {
-    window.localStorage.setItem('email',  $('#email').val());
+    window.localStorage.setItem('username',  $('#username').val());
+    let email = $('#username').val()+'@helpdeskti.com.br';
+    window.localStorage.setItem('email', email);
     window.localStorage.setItem('password',  $('#password').val());
     event.preventDefault();
     $(location).attr('href', 'home.html');
   });
+});
+
+$(document).ready(function(){
+  let message = 'Bem-vindo, ';
+  $("#username_Field").text(message+window.localStorage.getItem('username'));
+  $("#email_Field").text(window.localStorage.getItem('email'));
 });
