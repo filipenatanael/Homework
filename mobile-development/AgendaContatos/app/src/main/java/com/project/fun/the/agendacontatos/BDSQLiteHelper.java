1e303c51e3d618fbd4ca3e882f8816d0855cfa79
Filipe Natanael <filipenatanael@live.com>
2018-05-02 22:44:12 -0300

    Create method to add Contact

--- mobile-development/AgendaContatos/app/src/main/java/com/project/fun/the/agendacontatos/BDSQLiteHelper.java
+++ mobile-development/AgendaContatos/app/src/main/java/com/project/fun/the/agendacontatos/BDSQLiteHelper.java
@@ -1,5 +1,6 @@
 package com.project.fun.the.agendacontatos;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
@@ -13,6 +14,12 @@ public class BDSQLiteHelper extends SQLiteOpenHelper{
 
     private static final int DATABASE_VERSION = 1;
     private static final String  DATABASE_NAME = "ContatoDB";
+
+    private static final String TABELA_CONTATOS = "contatos";
+    private static final String ID = "id";
+    private static final String NOME = "nome";
+    private static final String TELEFONE = "telefone";
+    private static final String[] COLUNAS = { ID, NOME, TELEFONE };
 
     public BDSQLiteHelper(Context context) {
         super(context, DATABASE_NAME,  null,  DATABASE_VERSION);
@@ -33,4 +40,18 @@ public class BDSQLiteHelper extends SQLiteOpenHelper{
        db.execSQL("DROP TABLE IF EXISTS contatos");
         this.onCreate(db);
     }
+
+    public void addContato(Contato contato) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NOME, contato.getNome());
+        values.put(TELEFONE, contato.getTelefone());
+        db.insert(TABELA_CONTATOS, null, values);
+        db.close();
+    }
+
+
+
+
+
 }
