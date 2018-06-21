8bcc115fa7d12db6e8d7deb353d148259c516850
Filipe Natanael <filipenatanael@live.com>
2018-06-21 00:31:06 -0300

    Created OnItemSelected Method

--- mobile-development/App1/App1/App1/MainPage.xaml.cs
+++ mobile-development/App1/App1/App1/MainPage.xaml.cs
@@ -12,6 +12,48 @@ namespace App1
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+
+            Picker pickerMoedaDesejada = this.FindByName<Picker>("pickerMoedaDesejada");
+            List<string> Moedas = new List<string>();
+            Moedas.Add("USD");
+            Moedas.Add("EUR");
+            Moedas.Add("ARS");
+            Moedas.Add("GBP");
+            Moedas.Add("BTC");
+
+            pickerMoedaDesejada.ItemsSource = Moedas;
+        }
+
+        public void OnItemSelected(object sender, EventArgs e)
+        {
+            Picker primaryKey = (Picker)sender;
+            Entry entryValor = this.FindByName<Entry>("entryValorEmReais");
+
+            if (primaryKey.SelectedIndex != -1 && primaryKey.SelectedIndex > 0)
+            {
+                if (primaryKey.Items[primaryKey.SelectedIndex].Equals("Dolar"))
+                {
+                    entryValor.Text = "USD";
+                }
+                else if (primaryKey.Items[primaryKey.SelectedIndex].Equals("Euro"))
+                {
+                    entryValor.Text = "EUR";
+                }
+                else if (primaryKey.Items[primaryKey.SelectedIndex].Equals("Peso Argentino"))
+                {
+                    entryValor.Text = "ARS";
+                }
+                else if (primaryKey.Items[primaryKey.SelectedIndex].Equals("Libra Esterlina"))
+                {
+                    entryValor.Text = "GBP";
+                }
+                else if (primaryKey.Items[primaryKey.SelectedIndex].Equals("Betcoin"))
+                {
+                    entryValor.Text = "BTC";
+                }
+            }
+        }
+
+
+    }
 }
